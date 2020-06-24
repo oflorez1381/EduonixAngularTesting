426b25e69d3c3d7107941ca72baa7bcab32a9bca
oflorez <v-oflorez@expedia.com>
2020-06-23 22:15:36 -0400

    7.1 Protractor configuration

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,7 +1,7 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {AppPage} from './app.po';
+import {browser, logging} from 'protractor';
 
-describe('workspace-project App', () => {
+describe('angular-testing app', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('EduonixAngularTesting app is running!');
+    expect(page.getTitleText()).toEqual('Welcome to EduonixAngularTesting!');
   });
 
   afterEach(async () => {
